e5b5d7f0da92a45f8e58a3c50b60135e4fa70262
mikiehenning <mh08950@georgiasouthern.edu>
2017-11-04 17:49:40 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import TestPage from './testPage';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<TestPage />, document.getElementById('root'));
-registerServiceWorker();
