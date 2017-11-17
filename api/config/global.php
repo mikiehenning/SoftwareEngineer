7b6514d38e2e8445e35733a06f7eb9eb38cc1218
Zach Rider <zrider99zr@gmail.com>
2017-11-17 15:02:37 -0500

    Added an api

---
+++ api/config/global.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+define('DB_HOST', '165.227.191.245');
+define('DB_USER', 'project');
+define('DB_PASS', 'goeagles');
+define('DB_NAME', 'corstrata');
+define('SESSION_LENGTH', 60);
+?>
